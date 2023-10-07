6f52723a427e99c1a56b5b9e74a3a0feab7847f8
ddsr <dam2600@gmail.com>
2023-10-06 22:52:16 -0700

    ejemplo de palindromo agregado

--- src/CollectionTest.java
+++ src/CollectionTest.java
@@ -1,8 +1,10 @@
 import exercises.FindFactorial;
+import exercises.Palindrome;
 
 public class CollectionTest {
     public static void main (String [] args) {
 //        new ControllerCollection();
-        new FindFactorial();
+//        new FindFactorial();
+        new Palindrome();
     }
 }
