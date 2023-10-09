7890f6d2bce062e655abfb1018cd1711a1266be2
DDSR <damian@d2600.com>
2023-10-09 15:36:30 -0700

    agregando ejemplo de numero en reversa

--- src/CollectionTest.java
+++ src/CollectionTest.java
@@ -1,12 +1,14 @@
 import exercises.Anagram;
 import exercises.FindFactorial;
 import exercises.Palindrome;
+import exercises.ReversNumber;
 
 public class CollectionTest {
     public static void main (String [] args) {
 //        new ControllerCollection();
 //        new FindFactorial();
 //        new Palindrome();
-        new Anagram();
+//        new Anagram();
+        new ReversNumber(987654);
     }
 }
