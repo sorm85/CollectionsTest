51d89bf04d414306c5cec94bbb9eba0ba1575871
ddsr <dam2600@gmail.com>
2023-10-16 08:43:33 -0700

    Agregando una función para un anagrama con abecedario, letra por letra

--- src/exercises/Anagram.java
+++ src/exercises/Anagram.java
@@ -4,12 +4,17 @@ import java.util.Scanner;
 
 public class Anagram {
     public Anagram() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Escribe la 1ra palabra: ");
-        String str1 = scan.nextLine();
-        System.out.println("Escribe la 2da palabra: ");
-        String str2 = scan.nextLine();
-        System.out.println("Las palabras "+isAnagram(str1,str2));
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Escribe la 1ra palabra: ");
+//        String str1 = scan.nextLine();
+//        System.out.println("Escribe la 2da palabra: ");
+//        String str2 = scan.nextLine();
+//        System.out.println("Las palabras "+isAnagram(str1,str2));
+
+
+//        abecedario();
+
+        System.out.println("el resultado es : "+isAnagram2("aaabbb","bbaaa"));
     }
 
     private String isAnagram(String str1, String str2) {
@@ -32,6 +37,53 @@ public class Anagram {
 
 
         return response;
+    }
+    protected void abecedario () {
+        char[] s;
+        s=new char[26];
+        for ( int i=0; i<26; i++) {
+            s[i] = (char) ('A' + i );
+        }
+        for (int i = 0; i < s.length; i++) {
+            System.out.println(s[i]);
+        }
+    }
+    static boolean isAnagram2(String a, String b) {
+
+        if (a.length() != b.length()){
+//            System.out.println("return false en length");
+            return false;
+        }
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+        char [] str1 = a.toCharArray();
+        char [] str2 = b.toCharArray();
+
+        char [] abc = new char [26];
+        for (int i = 0; i < abc.length; i++) {
+            abc[i] = (char) ('a' + i);
+        }
+        for (int i = 0; i < abc.length; i++) {
+            int indexA = 0;
+            int indexB = 0;
+
+            for (int j = 0; j < str1.length; j++) {
+                if (abc[i] == str1[j]){
+                    indexA++;
+                }
+            }
+            for (int j = 0; j < str2.length; j++) {
+                if (abc[i] == str2[j]){
+                    indexB++;
+                }
+            }
+            if(indexA != indexB){
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
